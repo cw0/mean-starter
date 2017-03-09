970c5ba11bbfd3c619c7fd30293c6e36fa725f4d
cw0 <element4emerica@gmail.com>
2017-03-08 22:48:19 -0500

    config updates

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,54 @@
 module.exports = function(app) {
-
+	var Nerd = require('./models/Nerd');
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
+	app.get('/api/nerds', function(req, res) {
+		Nerd.find(function(err, nerds) {
+			if (err) {
+				res.send(err);
+			}
+
+			res.json(nerds);
+		});
+	});
+
+	app.post('/api/nerds', function(req, res) {
+		Nerd.create({
+			name : req.body.name,
+			done : false
+		}, function(err, nerd) {
+			if (err) {
+				res.send(err);
+			}
+
+			Nerd.find(function(err, nerds) {
+				if (err) {
+					res.send(err);
+				}
+
+				res.json(nerds);
+			});
+		});
+	});
+
+	app.delete('/api/nerds/:nerd_id', function(req, res) {
+		Nerd.remove({
+			_id : req.params.nerd_id
+		}, function (err, nerd) {
+			if (err) {
+				res.send(err);
+			}
+
+			Nerd.find(function(err, nerds) {
+				if (err) {
+					res.send(err);
+				}
+
+				res.json(nerds);
+			})
+		})
+	})
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,5 +1,30 @@
-angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
+angular.module('NerdCtrl', []).controller('NerdController', function($scope, $http) {
 
 	$scope.tagline = 'Nothing beats a pocket protector!';
+
+	$scope.formData = {};
+
+	$http.get('/api/nerds')
+		.then(function(data) {
+			$scope.nerds = data.data;
+			console.log(data);
+		});
+
+	$scope.createNerd = function() {
+		$http.post('/api/nerds', $scope.formData)
+			.then(function(data) {
+				$scope.formData = {};
+				$scope.nerds = data.data;
+				console.log(data);
+			});
+	}
+
+	$scope.deleteNerd = function(id) {
+		$http.delete('/api/nerds/' + id)
+			.then(function(data) {
+				$scope.nerds = data.data;
+				console.log(data);
+			});
+	};
 
 });
